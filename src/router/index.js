41c407f608a7939f7dc78ee513d1d7e606bc607e
ibocodes <abokiyaro8@gmail.com>
2025-10-28 02:39:29 +0100

    chore: fix import casing and add wave asset

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Landing from '../pages/Landing.vue'
-import Login from '../pages/auth/Login.vue'
-import Signup from '../pages/auth/Signup.vue'
-import Dashboard from '../pages/Dashboard.vue'
-import Tickets from '../pages/Tickets.vue'
+import Landing from '../pages/landing.vue'
+import Login from '../pages/auth/login.vue'
+import Signup from '../pages/auth/signup.vue'
+import Dashboard from '../pages/dashboard.vue'
+import Tickets from '../pages/tickets.vue'
 
 const routes = [
   { path: '/', component: Landing },
